591bdae73d0e43fa769d5b1749277360f7030bb3
patrick-nelissen <pnelissen@yahoo.com>
2023-01-20 19:28:40 -0800

    First commit
    Utility used on the PC side to send command over RS485 to the motion controller

---
+++ Utilities/SendRs485Command.py
@@ -0,0 +1,44 @@
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial("COM16", 57600, timeout=0, bytesize=8, parity="N", stopbits=1)
+
+ser.isOpen()
+
+
+
+# while True:
+#     string = "HELLO\n\r"
+#     print(string)
+#     # send the character to the device
+#     # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+#     ser.write(bytes(string, "utf-8"))
+#     time.sleep(1)
+
+print( 'Enter your serial commands below.\r\nInsert "exit" to leave the application.')
+
+keyboardInput = 1
+while 1 :
+    # get keyboard input
+    keyboardInput = input("Enter Command: ")
+        # Python 3 users
+        # input = input(">> ")
+    if keyboardInput == "exit":
+        ser.close()
+        exit()
+    else:
+        string = bytes(keyboardInput, "utf-8")
+        # send the character to the device
+        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
+        ser.write(string)
+        out = ""
+        outString = ""
+        # let's wait 0.01 second before reading output (let's give device time to answer)
+        time.sleep(0.01)
+        while ser.inWaiting() > 0:
+            out = ser.read(1)
+            outString += out.decode()
+            
+        if outString != "":
+            print( "Serial Reply: " + outString)
