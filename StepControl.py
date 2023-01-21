b7d345677c9c3aa5096b7db57c2502704fa6d712
Patrick Nelissen <pnelissen@yahoo.com>
2023-01-20 18:25:36 -0800

    Add more commands

--- StepControl.py
+++ StepControl.py
@@ -1,6 +1,5 @@
 # RP2040
 import machine
-import gc
 from machine import Pin
 
 # Time
@@ -8,6 +7,12 @@ from utime import sleep, sleep_ms, sleep_us, ticks_us
 
 # Math
 from math import sqrt
+
+# Garbage collection
+# NOTE: We do a lot of square root calculations that create
+#       a lot of garbage that requires on demand collection
+#
+import gc
 
 
 class Stepper:
@@ -19,34 +24,38 @@ class Stepper:
     Home = Pin(5, Pin.IN)
 
     # Defaults
-    ss = 0.137441         # Stepsize in mm
-    vmax = 1000           # Maximum desired linear velocity in mm/s
-    acc = 10000           # Acceleration in mm/(s*s)
-    deltaS = 20*109.95574 # Distance to travel in mm
-    hS = 100              # Maximum distance to travel in mm for finding home flag
-
-
+    ss     = 0.137  # Stepsize in mm
+    vmax   = 1000.0 # Maximum desired linear velocity in mm/s
+    acc    = 2000.0 # Acceleration in mm/s2
+    deltaS = 100.0  # Distance to travel in mm
+    hS     = 100.0  # Maximum distance to travel in mm when seeking home flag
+    absPos = -1.0   # Absolute position in mm (negative when invalid)
+    absCnt = -1     # Absolute stepcount in (micro)steps (negative when invalid)
+    
+    StepDelays = []
+    Homed = False
+        
     def _init_(self):
-        self.EnableDriver()
+        self.DisableDriver()
         self.Homed = False
-        self.StepDelays = []
     
     def EnableDriver(self):
         self.Ena.value(0)
+        print("Driver enabled")
         
     def DisableDriver(self):
-        self.Ena.value(1) 
+        self.Ena.value(1)
+        print("Driver disabled")
 
     def SetStepDirection( self, StepDirection = "CW" ):
         
         self.StepDirection = StepDirection
+        print("Step direction set to: ", self.StepDirection)
         
-        if self.StepDirection == "CW":
+        if StepDirection == "CW":
             self.Dir.value(1)
-        elif self.StepDirection == "CCW":
+        elif StepDirection == "CCW":
             self.Dir.value(0)
-        else:
-            print("Undefined step direction: ", StepDirection)
 
     def SetStepSize(self, ss):
     
@@ -72,23 +81,20 @@ class Stepper:
     
         self.acc = acc
         print("Acceleration set to: ", self.acc, " mm/s2")
-       
-    def SetMaxHomeMoveDistance(self, hS):
-    
-        self.hS = hS
-        print("Max home move distance set to: ", self.acc, " mm/s2")     
-
+           
     def HomeAxis(self, hS):
         
-        # Home distance is maximum distnace teh stepper will move to find home flag
+        # Home distance is maximum distance the stepper will move to find home flag
         self.hS = hS
+        
+        print("Home Flag: ", self.Home.value())
         
         # Are we in the home flag already?
         if self.Home.value() == 1:
             
             # Move out of the flag by 4.0 mm
             self.SetStepDirection( "CW" )       
-            absSteps = round(abs(10.0) / self.ss) # Calc number of steps we need to do
+            absSteps = round(10.0 / self.ss) # Calc number of steps we need to do
             
             # Take 1 ms steps
             for incStep in range(absSteps):
@@ -114,6 +120,8 @@ class Stepper:
             if self.Home.value() == 1:
                 print("Home found!")
                 self.Homed = True
+                self.absPos = 0.0
+                self.absCnt = 0
                 break
 
         # Reached th eend of the home move?
@@ -122,12 +130,19 @@ class Stepper:
                 
     def CalcMove(self):
         
+        # Clear previous calculated motion delays
+        self.StepDelays.clear()
+        
+        # Set the target vmax for this move.
+        # Dependiong on the acceleration profile the target vmax might get adjusted
+        vmax_target = self.vmax
+        
         absSteps = round(abs(self.deltaS) / self.ss) # number of steps we need to do
 
         print("absSteps: ", absSteps)
 
         # define end point of acceleration and start point of deceleration
-        s_1 = self.vmax * self.vmax / (2 * self.acc)
+        s_1 = vmax_target * vmax_target / (2 * self.acc)
         s_2 = self.deltaS - s_1
 
         print("s_1: ", s_1)
@@ -136,11 +151,11 @@ class Stepper:
         if s_1 > s_2: # if we dont even reach full speed
             s_1 = self.deltaS / 2
             s_2 = self.deltaS / 2;
-            vmax = sqrt(self.deltaS * self.acc)
+            vmax_target = sqrt(self.deltaS * self.acc)
 
         print("s_1_m: ", s_1)
         print("s_2_m: ", s_2)
-        print("vmax: ", vmax)
+        print("vmax target: ", vmax_target)
 
         vcurr = 0
 
@@ -164,7 +179,7 @@ class Stepper:
             elif s < s_2:
                 vcurr = self.vmax
             else:
-                vcurr = sqrt( self.vmax * self.vmax - 2 * (self.s - s_2) * self.acc)
+                vcurr = sqrt( self.vmax * self.vmax - 2 * (s - s_2) * self.acc)
                
             # convert velocity to delay
             tDelay = round(self.ss / vcurr * 1e6) # in micro seconds
@@ -182,8 +197,16 @@ class Stepper:
 
         if self.Homed :
 
-            self.SetStepDirection("CW")
-
+            if self.StepDirection == "CW":
+                self.Dir.value(1)
+            elif self.StepDirection == "CCW":
+                self.Dir.value(0)
+            else:
+                print("Undefined step direction: ", StepDirection)
+                
+                # Default to CW
+                self.Dir.value(1)
+            
             # Now step through the list of pre-calculated delsyas and genere the pulse train to execute the motion
             for StepDelay in self.StepDelays:
                     
@@ -194,6 +217,4 @@ class Stepper:
                 sleep_us(StepDelay)
         else:
             print("Axis has not been homed!")
-
-
 
